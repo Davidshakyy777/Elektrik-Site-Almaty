9c29ca7a720796e827415e27bcd3d7b551b22cee
Davidshakyy777 <Davidshakyy777@gmail.com>
2026-02-03 13:31:46 +0500

    Initial site

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const chat = document.querySelector('.chat');
 const menu = document.querySelector('.chat-menu');
 
-chat.addEventListener('click', () => {
-  menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+// Чатты басқанда ашу / жабу
+chat.addEventListener('click', (e) => {
+  e.stopPropagation(); // click жоғарыға өтпесін
+  menu.classList.toggle('show');
+});
+
+// Чат менюін басқанда жабылмасын
+menu.addEventListener('click', (e) => {
+  e.stopPropagation();
+});
+
+// Беттiң басқа жерiн басқанда жабу
+document.addEventListener('click', () => {
+  menu.classList.remove('show');
 });
