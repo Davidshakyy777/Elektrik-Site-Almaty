f85cc983c677976ca77e2ef5f16bb1827c578866
Davidshakyy777 <Davidshakyy777@gmail.com>
2026-02-05 02:17:06 +0500

    site redesign responsive mobile fix

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-// Chat батырмасы
 const chat = document.querySelector('.chat');
-const menu = document.querySelector('.chat-menu');
+  const menu = document.querySelector('.chat-menu');
 
-chat.addEventListener('click', (e) => {
-  e.stopPropagation();
-  menu.classList.toggle('show');
-});
+  chat.addEventListener('click', (e) => {
+    e.stopPropagation();
+    menu.classList.toggle('show');
+  });
 
-// Chat-тан тыс жерді басқанда жабу
-document.addEventListener('click', (e) => {
-  if (!chat.contains(e.target)) {
-    menu.classList.remove('show');
-  }
-});
+  // Chat-тан тыс жерді басқанда жабу
+  document.addEventListener('click', (e) => {
+    if (!chat.contains(e.target)) {
+      menu.classList.remove('show');
+    }
+  });
